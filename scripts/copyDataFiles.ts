78e8348bfd4850edda6243b1718b9cf4b25b7c2b
deif <deif@pm.me>
2023-11-27 20:36:02 +0000

    attempt to make copy file scripts cross-os compatible

--- scripts/copyDataFiles.ts
+++ scripts/copyDataFiles.ts
@@ -1,12 +1,17 @@
 
 import { copyFile } from "fs/promises"
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 async function main() {
-    await copyFile('../estfor-contracts/scripts/data/actions.ts', './src/data/actions.ts')
+    await copyFile(join(__dirname, '../estfor-contracts/scripts/data/actions.ts'), join(__dirname, '../src/data/actions.ts'))
 
-    await copyFile('../estfor-contracts/scripts/data/items.ts', './src/data/items.ts')
+    await copyFile(join(__dirname, '../estfor-contracts/scripts/data/items.ts'), join(__dirname, '../src/data/items.ts'))
 
-    await copyFile('../estfor-contracts/scripts/data/actionChoices.ts', './src/data/actionChoices.ts')
+    await copyFile(join(__dirname, '../estfor-contracts/scripts/data/actionChoices.ts'), join(__dirname, '../src/data/actionChoices.ts'))
 }
 
 main().catch((e) => {
