292f52271df65a1530142881b2a8309305522fde
deif <deif@pm.me>
2024-04-21 12:13:03 +0100

    add select only ready button for factory

--- src/components/factory/AssignedSilos.vue
+++ src/components/factory/AssignedSilos.vue
@@ -4,41 +4,47 @@
     >
         <div class="card-body">
             <h2 class="text-2xl font-bold text-center">Assigned Heroes</h2>
-            <div class="flex items-center justify-end">
-                <div class="join items-center justify-end">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="w-6 h-6 mr-2 text-primary"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+            <div class="flex items-center justify-between">
+                <button type="button" class="btn btn-primary btn-sm" @click="selectAllReady">
+                    Select Only Ready
+                </button>
+            
+                <div class="flex items-center justify-end">
+                    <div class="join items-center justify-end">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="w-6 h-6 mr-2 text-primary"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                            />
+                        </svg>
+                        <input
+                            type="text"
+                            placeholder="Search"
+                            class="input input-bordered bg-base-100-50 input-sm"
+                            v-model="searchValue"
                         />
-                    </svg>
-                    <input
-                        type="text"
-                        placeholder="Search"
-                        class="input input-bordered bg-base-100-50 input-sm"
-                        v-model="searchValue"
+                    </div>
+                    <AssignedHeroSkillSelect
+                        class="ms-2"
+                        v-model="selectedSkillGroup"
+                        :heroes="factoryStore.assignedProxys"
+                        @update:modelValue="pageNumber = 0"
+                    />
+                    <AssignedHeroGroupSelect
+                        class="ms-2"
+                        v-model="selectedHeroGroup"
+                        :heroes="factoryStore.assignedProxys"
+                        @update:modelValue="pageNumber = 0"
                     />
                 </div>
-                <AssignedHeroSkillSelect
-                    class="ms-2"
-                    v-model="selectedSkillGroup"
-                    :heroes="factoryStore.assignedProxys"
-                    @update:modelValue="pageNumber = 0"
-                />
-                <AssignedHeroGroupSelect
-                    class="ms-2"
-                    v-model="selectedHeroGroup"
-                    :heroes="factoryStore.assignedProxys"
-                    @update:modelValue="pageNumber = 0"
-                />
             </div>
             <div class="overflow-x-auto">
                 <table class="table md:table-md table-xs">
@@ -322,6 +328,22 @@ const pagesToSelect = computed(() => {
 const selectAllSilos = () => {
     for (const silo of assignedSilosRef.value) {
         silo.selected = selectAll.value
+    }
+}
+
+const selectAllReady = () => {
+    for (const silo of assignedSilosRef.value) {
+        let ready = true
+        if (silo.queuedActions) {
+            for (const a of silo.queuedActions) {
+                const s = calculateTimeLeft(a)
+                if (s !== "Ready") {
+                    ready = false
+                    break
+                }
+            }
+        }
+        silo.selected = ready
     }
 }
 
